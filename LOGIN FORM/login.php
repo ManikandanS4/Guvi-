3f9478b9e72c7929ad027307d02955eaec372d8a
ManikandanS4 <95734405+ManikandanS4@users.noreply.github.com>
2022-11-03 10:29:33 +0530

    Add files via upload

---
+++ LOGIN FORM/login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>GUVI LOGIN</title>
+        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"> 
+        
+        <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <div class="container h-100">
+            <div class ="d-flex justify-content-center h-100">
+                <div class="user_card">
+                <div class ="d-flex justify-content-center">
+                    <div class="brand_logo_container">
+                        <img src="img/img1.png"class="brand_logo" alt="Programming Knowledge logo">
+
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js"
+             integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+
+             <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" >
+                </script>
+
+    </body>
+</html>
